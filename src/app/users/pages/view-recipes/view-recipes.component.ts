bebe8cb81aaef1cb6ba9923654bb502d5f9e364b
AyishaFahma <mailtofahma@gmail.com>
2025-07-26 13:59:57 +0530

    frontend completed

--- src/app/users/pages/view-recipes/view-recipes.component.ts
+++ src/app/users/pages/view-recipes/view-recipes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-view-recipes',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent , RouterLink],
   templateUrl: './view-recipes.component.html',
   styleUrl: './view-recipes.component.css'
 })
